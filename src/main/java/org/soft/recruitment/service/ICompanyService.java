673866e06fdd40529a7ec0d410bb8a06c079626d
Administrator <YoYoYang>
2020-04-11 20:41:48 +0800

    s

--- src/main/java/org/soft/recruitment/service/ICompanyService.java
+++ src/main/java/org/soft/recruitment/service/ICompanyService.java
@@ -5,31 +5,31 @@ import org.soft.recruitment.model.Company;
 import java.util.List;
 
 
-interface ICompanyService {
-    //公司注册
-    void companyRegister(Company company);
+public interface ICompanyService {
+	//公司注册
+	public void companyRegister(Company company);
 
-    //公司登录
-    Company companyLogin(String companyLoginName, String companyLoginPassword);
+	//公司登录
+	public Company companyLogin(String companyLoginName , String companyLoginPassword);
 
-    //公司修改信息(基础信息和密码)
-    void updateCompany(Integer companyId, Company company);
+	//公司修改信息(基础信息和密码)
+	public void updateCompany(Integer companyId , Company company);
 
-    //删除公司
-    void deleteCompany(Integer companyId);
+	//删除公司
+	public void deleteCompany(Integer companyId);
 
-    //查询所有公司,增加查询条件公司名
-    List<Company> findAllCompany(int page, int size, String companyName);
+	//查询所有公司,增加查询条件公司名
+	public List<Company> findAllCompany(int page, int size, String companyName);
 
-    //根据公司ID查询公司
-    Company findCompanyByCompanyId(Integer page, Integer size, Integer companyId);
+	//根据公司ID查询公司
+	public  Company findCompanyByCompanyId(Integer page, Integer size, Integer companyId);
 
-    //查询公司登录名（校验登录名是否存在）
-    int findCompanyByCompanyLoginName(String companyLoginName);
+	//查询公司登录名（校验登录名是否存在）
+	public int findCompanyByCompanyLoginName(String companyLoginName);
 
-    //查询公司名称（校验公司名是否存在）
-    int findCompanyByCompanyName(String companyName);
+	//查询公司名称（校验公司名是否存在）
+	public int findCompanyByCompanyName(String companyName);
 
-    //保存公司信息
-    void saveCompany(Company company);
+	//保存公司信息
+	public void saveCompany(Company company);
 }
--- src/main/java/org/soft/recruitment/service/IUserService.java
+++ src/main/java/org/soft/recruitment/service/IUserService.java
@@ -5,35 +5,35 @@ import org.soft.recruitment.model.User;
 import java.util.List;
 
 //用户下所拥有的接口
-interface IUserService {
+public interface IUserService {
     //  用户注册
-    void userRegister(User user);
+    public void userRegister(User user);
 
     //  用户登录
-    User userLogin(String userLoginName, String userLoginPassword);
+    public User userLogin(String userLoginName, String userLoginPassword);
 
     //  用户修改密码
-    void updateUserPassword(Integer userId, User User);
+    public void updateUserPassword(Integer userId, User User);
 
     // 查询所有用户
-    List<User> findAllUser();
+    public List<User> findAllUser();
 
     //根据ID查询用户
-    User findUserByUserId(Integer userId);
+    public User findUserByUserId(Integer userId);
 
     // 根据用户登录名查询用户（校验登录名是否存在）
-    int findUserByUserLoginName(String userLoginName);
+    public int findUserByUserLoginName(String userLoginName);
 
     // 根据用户真实姓名查找用户（校验真实姓名是否存在）
-    User findUserByUserRealName(String userRealName);
+    public User findUserByUserRealName(String userRealName);
 
     //  根据ID删除用户
-    void deleteByUserId(Integer userId);
+    public void deleteByUserId(Integer userId);
 
     //  根据ID修改用户信息
-    void updateUser(Integer userId, User User);
+    public void updateUser(Integer userId, User User);
 
     //   保存用户信息
-    void saveUser(User user);
+    public void saveUser(User user);
 
 }
